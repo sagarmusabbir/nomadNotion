67c4a2fe1601f2bb9c1426d5a025d1e278039272
sagarmusabbir <sagarmusabbir@gmail.com>
2024-07-14 03:42:17 +0600

    tailgraph-og

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,16 @@ export const metadata: Metadata = {
 
     description: siteData.headerDescription,
 
-    images: [`/api/og?title=${siteData.title}`],
+    images: [
+      // `/api/og?title=${siteData.title}`
+      {
+        // url: "https://og.tailgraph.com/og?fontFamily=Inter&title=Musabbirs%20Terminal&titleTailwind=text-gray-800%20font-bold%20text-6xl&text=Creator%20of%20Third%20Bracket%20Solutions%2C%20Nomad%20Gang%20Labs%20and%20Products%20of%20Bangladesh.&textTailwind=text-gray-700%20text-2xl%20mt-4&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2FnomadNotion%2Fmain%2Fpublic%2FLogo.svg&logoTailwind=w-14%20mx-auto&bgUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2FnomadNotion%2Fmain%2Fpublic%2Fmusabbir-cover.svg&bgTailwind=bg-white&footer=musabbirsagar.com&footerTailwind=text-emerald-400&t=1720906704239&refresh=1",
+        url: "https://og.tailgraph.com/og?fontFamily=Roboto&title=Hub%20for%20Made%20in%20Bangladesh%20Products&titleTailwind=text-white%20font-bold%20text-center%20text-5xl%20antialiased%20text-wrap&titleFontFamily=Inter&text=Connecting%20global%20sellers%20and%20brands%20with%20top%20manufacturers%20and%20suppliers%20from%20bangladesh&textTailwind=text-white%20antialiased%20text-xl%20text-center&textFontFamily=Inter&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2Fproductsofbangladesh.com%2Fmain%2Fpublic%2Foglogo.svg&logoTailwind=opacity-100%20border%20mx-auto%20rounded-lg%20pl-2%20pt-2%20pb-2%20border-gray-200%20bg-transparent&bgUrl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Ff%2Ff9%2FFlag_of_Bangladesh.svg%2F510px-Flag_of_Bangladesh.svg.png&bgTailwind=bg-white%20bg-no-repeat%20bg-cover%20bg-center%20bg-opacity-70&overlay=1&overlayTailwind=bg-blueGray-400%20bg-gray-500%20bg-gray-700%20opacity-50&footer=productsofbangladesh.com&footerTailwind=text-teal-600%20text-xl&containerTailwind=p-4%20overflow-hidden&t=1720668348785&refresh=1",
+        alt: "musabbirsagar.com og-image",
+        width: 800,
+        height: 600,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
@@ -53,7 +62,15 @@ export const metadata: Metadata = {
 
     description: siteData.headerDescription,
 
-    images: [`/api/og?title=${siteData.title}`],
+    images: [
+      {
+        // url: "https://og.tailgraph.com/og?fontFamily=Roboto&title=Best%20Platform%20for%20sourcing%20Made%20in%20Bangladesh%20Products&titleTailwind=font-extrabold%20text-white%20text-5xl%20text-center%20px-4&titleFontFamily=Inter&text=ProductsofBangladesh.com&textTailwind=text-2xl%20mt-4%20text-center%20text-white&textFontFamily=Inter&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2Ftb%2Fmain%2Fpublic%2Foldlogo.svg&logoTailwind=text-center%20mx-auto%20w-20%20h-20%20p-4&bgUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2Ftb%2Fmain%2Fpublic%2Fcover-mint.svg&bgTailwind=bg-white%20bg-opacity-50&footer=ProductsofBangladesh.com&footerTailwind=text-white&containerTailwind=p-4&t=1720553933274&refresh=1",
+        url: "https://og.tailgraph.com/og?fontFamily=Inter&title=Musabbirs%20Terminal&titleTailwind=text-gray-800%20font-bold%20text-6xl&text=Creator%20of%20Third%20Bracket%20Solutions%2C%20Nomad%20Gang%20Labs%20and%20Products%20of%20Bangladesh.&textTailwind=text-gray-700%20text-2xl%20mt-4&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2FnomadNotion%2Fmain%2Fpublic%2FLogo.svg&logoTailwind=w-14%20mx-auto&bgUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2FnomadNotion%2Fmain%2Fpublic%2Fmusabbir-cover.svg&bgTailwind=bg-white&footer=musabbirsagar.com&footerTailwind=text-emerald-400&t=1720906704239&refresh=1",
+        alt: "musabbirsagar.com og-image",
+        width: 800,
+        height: 600,
+      },
+    ],
   },
 };
 
