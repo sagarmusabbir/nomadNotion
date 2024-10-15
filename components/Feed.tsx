501b920c74d9be5b6fc699829b0a27214ed3374c
sagarmusabbir <sagarmusabbir@gmail.com>
2024-10-16 03:31:36 +0600

    fix-error

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -5,19 +5,15 @@ import ProjectCard from "./ProjectCard";
 
 type Props = {
   articles: Article[];
-  projects: Project[];
+  // projects: Project[];
 };
 
-export default function ArticleList({ articles, projects }: Props) {
+export default function ArticleList({ articles }: Props) {
   return (
     <div className="grid gap-10 lg:gap-12 sm:grid-cols-2">
       {articles &&
         articles.map((article) => (
           <ArticleCard article={article} key={article.id} />
-        ))}
-      {projects &&
-        projects.map((project) => (
-          <ProjectCard project={project} key={project.id} />
         ))}
     </div>
   );
