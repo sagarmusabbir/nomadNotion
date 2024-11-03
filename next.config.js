940800c508aefddf4cd2d80d9ff839578bf8fe4a
MUSABBIR SAGAR <56739377+sagarmusabbir@users.noreply.github.com>
2024-11-03 19:42:59 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    unoptimized: true,
     // domains: ["prod-files-secure.s3.us-west-2.amazonaws.com", "unsplash.com"],
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
