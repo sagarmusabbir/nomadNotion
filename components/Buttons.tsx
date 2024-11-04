ffba0ba6c61b88640ab4f44e2a6edd3f55bc8492
MUSABBIR SAGAR <56739377+sagarmusabbir@users.noreply.github.com>
2024-11-04 17:26:35 +0600

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -24,7 +24,7 @@ export const DarkButton: React.FC<ButtonProps> = ({ href, text, target }) => (
   <Link
     href={href}
     target={target}
-    className="border border-gray-900 rounded-md bg-gray-900 hover:bg-gray-800 focus:ring-1 focus:ring-slate-400 enabled:hover:bg-slate-50 items-center group relative flex justify-center px-4 py-2 text-center transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none leading-none drop-shadow-sm"
+    className="border border-gray-900 rounded-md bg-gray-900 hover:bg-gray-800 focus:ring-1 focus:ring-slate-400 enabled:hover:bg-slate-50 items-center group relative flex justify-center px-5 py-2.5 text-center transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none leading-none drop-shadow-sm"
   >
     <span className="font-semibold flex justify-center items-center text-sm lg:text-base leading-none text-white">
       {text}
